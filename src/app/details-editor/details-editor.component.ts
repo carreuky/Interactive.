98bfa923c868cc2feacdf9c253660dc1720b0308
kelvin <carreuky>
2023-02-16 10:45:25 +0300

    using pipes

--- src/app/details-editor/details-editor.component.ts
+++ src/app/details-editor/details-editor.component.ts
@@ -8,17 +8,22 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class DetailsEditorComponent {
   array: any[]= []
+
+  birthday = new Date(); // April 15, 1988 -- since month
   detailForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
     location: new FormControl('', [Validators.required])
  })
-
  
   onSubmit() {
     this.array.push(this.detailForm.value)
     console.log(this.array)
     this.detailForm.reset()
   }
+  toggle = true; // start with true == shortDate
+
+  get format()   { return this.toggle ? 'shortDate' : 'fullDate'; }
+  toggleFormat() { this.toggle = !this.toggle; }
 
  
 
